79e377a28fb9ac88954ba199f448a9d4fe87b53d
zacharyeller13 <zacharyeller@pm.me>
2021-09-21 20:39:54 -0500

    Refactor playlist_generator.py into  functions Fixes #8

--- playlist_generator.py
+++ playlist_generator.py
@@ -30,7 +30,30 @@ def get_music_library():
 
     return music_library, library_artists
 
-music_library, library_artists = get_music_library()
+def search_tracks(track, library_artists, music_library):
+    """Read in track info and list of library artists;
+    search for matching tracks and return a best match    
+    """
+    mp3_list=[]
+    track_artist = track['track']['artists'][0]['name'] # take first/primary artist
+    if len(track['track']['artists']) > 1: # if there are featured artists
+        # setting cutoff of 70, seems to match pretty well for now
+        artist_matches = [dir for dir in library_artists if fuzz.partial_ratio(track_artist, dir.name) > 70]
+
+    else:
+        # index library_artists with index provided by tuple returned from process.extract
+        artist_matches = [library_artists[result[2]] for result in process.extract(track_artist, [dir.name for dir in library_artists])]
+
+    for artist_dir in artist_matches:
+        for mp3_file in artist_dir.rglob("*.mp3"):
+            mp3_list.append(mp3_file)
+
+    best_match = process.extractOne(track['track']['name'], [mp3.stem for mp3 in mp3_list])
+    
+    # Return best_match's relative path to the music_library, taking the index of the best match
+    # in the mp3_list to get the full PosixPath object, allowing use of the .relative_path() method
+
+    return mp3_list[best_match[2]].relative_to(music_library)
 
 # TODO: #2 Read in list of tracks from spotipy output
 # For now, we will hardcode a dict based on music I know I have
@@ -59,36 +82,16 @@ track_info = {
     ]
 }
 
+music_library, library_artists = get_music_library()
+
 # Open new playlist file and begin searching and writing matches to file
 playlist_name = input("Name for New Playlist: ")
 
 with open(f"{music_library}/{playlist_name}.m3u", 'a') as f:
     f.write("#EXTM3U\n")
 
-    # for each track, grab the top 3 artist matches
-    # we will probably change to some sort of ratio threshold instead, but top 3 works for now
-    # for tracks with featured artists, we use partial_ratio for a better match
+    # for each track search and return a best match based on a match ratio of 70 for artists
     for track in track_info['items']:
-        mp3_list = []
-        track_artist = track['track']['artists'][0]['name'] # take the first/primary artist
-        if len(track['track']['artists']) > 1: # checking if there are featured artists
-            # setting cuttoff of 70, seems to work for the most part right now
-            artist_matches = [dir for dir in library_artists if fuzz.partial_ratio(track_artist, dir.name) > 70]
+        best_match = search_tracks(track, library_artists, music_library)
 
-        else:
-            # index library_artists with index provided by the tuple returned from process.extract
-            artist_matches = [library_artists[result[2]] for result in process.extract(track_artist, [dir.name for dir in library_artists])]
-
-        # search each matched artist folder for mp3 files, recursively
-        # append all found mp3 files to a list mp3_list
-        for artist_dir in artist_matches:
-            for mp3_file in artist_dir.rglob("*.mp3"):
-                mp3_list.append(mp3_file)
-        
-        # Check each file in mp3_list against song from playlist
-        best_match = process.extractOne(track['track']['name'], [mp3.stem for mp3 in mp3_list])
-        
-        # write best_match relative path to the playlist file, taking the index of the best match
-        # in the mp3_list to get the full PosixPath object, allowing use of the .relative_path() method
-        mp3 = mp3_list[best_match[2]].relative_to(music_library)
-        f.write(f"{str(mp3)}\n")
+        f.write(f"{str(best_match)}\n")
