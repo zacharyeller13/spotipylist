3ff978f4e5262ea7b4c3a63247f99845d102ac80
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-03-17 14:07:22 -0500

    feat(music_library): Begin defining music_library module and class.
    Also added the base directory structure for tests.

---
+++ src/utils/music_library.py
@@ -0,0 +1,15 @@
+"""Module with a class to represent the user's music library."""
+
+class MusicLibrary:
+    """Represents a user's music library as as set of artists and tracks.
+
+    We will either pass in a filesystem and parse out the  artists and tracks,
+    or we will pass in the set of artists and tracks using some helper methods
+    in another module
+
+    Attritutes:
+        TODO
+    """
+    # TODO: Implement class
+    raise NotImplementedError
+
